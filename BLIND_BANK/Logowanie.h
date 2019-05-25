0fe84c6c7f390f151f0e6d2ccef481aed69e10ee
Kacp3r3 <kacper.sawicki.it@gmail.com>
2019-05-25 12:56:32 +0200

    90% of wykladowca

--- BLIND_BANK/Logowanie.h
+++ BLIND_BANK/Logowanie.h
@@ -186,7 +186,7 @@ namespace BLINDBANK {
 			this->txtIndex->Name = L"txtIndex";
 			this->txtIndex->Size = System::Drawing::Size(367, 38);
 			this->txtIndex->TabIndex = 8;
-			this->txtIndex->Text = L"gendzior@onet.pl";
+			this->txtIndex->Text = L"kacper@gmail.com";
 			// 
 			// button2
 			// 
